05c7711cc919caffe97df2076292edbcc461e7c9
DanCiciarello <me@danieleciciarello.com>
2022-07-01 19:34:53 +0200

    Create setup & installations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Import bootstrap
+import "bootstrap";
+
+// Importo the main scss
+import "./assets/scss/main.scss";
+
 Vue.config.productionTip = false
 
 new Vue({
