8d5b9536138454823dcd7c330815ddafba902423
DanCiciarello <me@danieleciciarello.com>
2022-07-03 23:20:33 +0200

    Update jumbohero

--- src/components/JumboHero.vue
+++ src/components/JumboHero.vue
@@ -2,19 +2,30 @@
     <div>
 
         <!-- Header USPs -->
-        <div>
-            <div class="row">
-                <div class="col-4"></div>
-                <div class="col-4"></div>
-                <div class="col-4"></div>
+        <div class="cdHeaderUsps">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 text-center" v-for="(item, i) in headerUSPs" :key="i">
+                        <i class="pe-3" :class="item.icon"></i>
+                        <span>{{item.text}}</span>
+                    </div>
+                </div>
             </div>
         </div>
 
         <!-- Hero -->
-        <div></div>
+        <div class="cdHero">
+            <div class="cdHeroOverlay">
+                <div class="container cdHeroWrapper d-flex align-items-center text-white">
+                    <h6>Find the best animal supplies</h6>
+                </div>
+            </div>
+        </div>
 
         <!-- Hero CTA -->
-        <div></div>
+        <div>
+
+        </div>
 
     </div>
 </template>
@@ -24,6 +35,26 @@ export default {
 
     // Name
     name: "JumboHero",
+
+    // Data
+    data(){
+        return {
+            headerUSPs: [
+                {
+                    icon: "fa-regular fa-map",
+                    text: "International Shipping Services"
+                },
+                {
+                    icon: "fa-regular fa-clock",
+                    text: "See our store hours and services"
+                },
+                {
+                    icon: "fa-solid fa-check",
+                    text: "We price match to give you the best deals"
+                },
+            ]
+        }
+    }
 
 }
 </script>
