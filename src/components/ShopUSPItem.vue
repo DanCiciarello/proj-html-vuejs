ee340cf80ab7c76e40479ac8397d55d7be2adce8
DanCiciarello <me@danieleciciarello.com>
2022-07-04 18:00:12 +0200

    Completed shop USPs component

---
+++ src/components/ShopUSPItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="text-white text-center py-5">
+
+        <i class="fa-2xl" :class="shopUspItem.icon"></i>
+        <p class="pt-3">{{shopUspItem.text}}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    // Name
+    name: "ShopUSPItem",
+
+    // Props
+    props: {
+        shopUspItem: Object,
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/ShopUSPs.vue
+++ src/components/ShopUSPs.vue
@@ -1,16 +1,49 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="cdShopUspContainer py-5">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-3" v-for="(item,i) in shopUspList" :key="i">
+                        <ShopUSPItem :shop-usp-item="item"/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
 </template>
 
 <script>
-export default {
 
+// Import
+import {state} from "../store";
+import ShopUSPItem from "./ShopUSPItem.vue";
+
+// Export
+export default {
+    
     // Name
     name: "ShopUSPs",
+    
+    // Computed
+    computed: {
+        shopUspList() {
+            return state.shopUspList;
+        }
+    },
+
+    // Components
+    components: { ShopUSPItem }
 
 }
 </script>
 
 <style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/shopusps";
 
 </style>
