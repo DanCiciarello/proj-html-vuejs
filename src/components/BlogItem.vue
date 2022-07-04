7893bbd52724961bf8aebd3ada7d28a561028efa
DanCiciarello <me@danieleciciarello.com>
2022-07-04 16:43:28 +0200

    Completed recent articles component

---
+++ src/components/BlogItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+        <div class="cdBlogItemImg">
+            <img :src="blogItem.img" :alt="blogItem.title">
+        </div>
+
+        <div class="cdBlogItemInfo mt-4">
+            <a href="#">
+                <h5>{{blogItem.title}}</h5>
+                <span class="cdBlogItemDate">{{blogItem.date}}</span>
+            </a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    // Name
+    name: "BlogItem",
+
+    // Props
+    props: {
+        blogItem: Object,
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/blogitem";
+
+</style>
--- src/components/RecentArticles.vue
+++ src/components/RecentArticles.vue
@@ -3,8 +3,22 @@
 
         <div class="container">
 
-            <h2>Get the best tips & tricks</h2>
-            
+            <!-- Section title -->
+            <div class="text-center pb-5">
+                <h2 class="pb-2">Get the best tips & tricks</h2>
+                <h5>Recent articles</h5>
+            </div>
+
+            <!-- Blog articles -->
+            <div class="row">
+                <div class="col-3" v-for="(item, i) in recentArticlesList" :key="i">
+                    <BlogItem :blog-item="item"/>
+                </div>
+            </div>
+
+            <div class="text-center py-5">
+                <button type="button" class="btn btn-dark">Read all articles</button>
+            </div>
 
         </div>
 
@@ -12,14 +26,36 @@
 </template>
 
 <script>
-export default {
 
+// Import
+import BlogItem from './BlogItem.vue';
+import {state} from "../store";
+
+// Export
+export default {
+    
     // Name
     name: "RecentArticles",
+
+    // Components
+    components: { BlogItem },
+
+    // Computed
+    computed: {
+        recentArticlesList(){
+            return state.recentArticlesList;
+        }
+    }
 
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+    @import "../assets/scss/variables";
+
+    h5 {
+        color: $tertiary;
+    }
 
 </style>
