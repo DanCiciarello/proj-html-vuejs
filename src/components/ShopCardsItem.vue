d51777e2c44bcba335d433a614a9d51c0c91d733
DanCiciarello <me@danieleciciarello.com>
2022-07-04 13:35:53 +0200

    Completed Shop Card Component

--- src/components/ShopCardsItem.vue
+++ src/components/ShopCardsItem.vue
@@ -4,7 +4,8 @@
         <div class="cdShopCard d-flex flex-column justify-content-center align-items-center">
             <h2 class="text-white">{{shopCardName}}</h2>
             <span class="cdShopCardDescription">{{shopCardDescription}}</span>
-            <img :src="shopCardImg" :alt="shopCardName" class="cdShopCardImg">
+            <img :src="shopCardImg" :alt="shopCardName" class="cdShopCardImg py-4">
+            <button type="button" class="btn btn-dark">{{shopCardBtnCta}}</button>
         </div>
 
     </div>
