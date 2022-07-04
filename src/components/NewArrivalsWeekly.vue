fa9adfbf6ad3e655e1d623783e216c9feecd7738
DanCiciarello <me@danieleciciarello.com>
2022-07-04 13:49:28 +0200

    Completed New Arrivals Component

--- src/components/NewArrivalsWeekly.vue
+++ src/components/NewArrivalsWeekly.vue
@@ -1,5 +1,17 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="container mt-5">
+            <div class="cdNewArrivalsWeekly">
+                <div class="cdOverlay d-flex flex-column align-items-center justify-content-center text-white">
+                    <h6>Find the best animal supplies</h6>
+                    <h1 class="pt-2 pb-4">New arrivals weekly</h1>
+                    <button type="button" class="btn btn-light">Learn more about us</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -12,5 +24,8 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/newarrivals";
 
 </style>
