4cf38f50c89cc48b04ba73f9d7357e15feead749
DanCiciarello <me@danieleciciarello.com>
2022-07-04 16:08:47 +0200

    Completed Join Newsletter component

--- src/components/JoinNewsletter.vue
+++ src/components/JoinNewsletter.vue
@@ -1,5 +1,24 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="container">
+            <div class="cdJoinNewsletter">
+                <!-- Overlay -->
+                <div class="cdJoinNewsletterOverlay d-flex align-items-center">
+                    <div class="cdJoinNewsletterText d-flex flex-column justify-content-center">
+                        <h2>Join our newsletter</h2>
+                        <p class="pt-3 pb-5">Volutpat vel turpis nulla lorem sed semper. Aliquam sagittis sem libero viverra vehicula nullam ut nisl.</p>
+                            <div class="input d-flex cdInputGroup">
+                                <input type="text" class="form-control cdInputGroupText" placeholder="Insert your email...*"
+                                    aria-describedby="addon-wrapping">
+                                <button type="button" class="btn btn-dark ms-3">Subscribe</button>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -12,5 +31,8 @@ export default {
 </script>
 
 <style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/joinnewsletter";
 
 </style>
