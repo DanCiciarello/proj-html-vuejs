96ca56f5424dc91cf77d6aa0bddad3a0741c6b51
DanCiciarello <me@danieleciciarello.com>
2022-07-04 18:37:15 +0200

    Completed shop boxes component

--- src/components/ShopBoxes.vue
+++ src/components/ShopBoxes.vue
@@ -5,7 +5,7 @@
             <div class="cdShopBox cdShopBoxLeft">
                 <div class="cdOverlay text-white d-flex flex-column justify-content-center align-items-center">
                     <h6>{{shopBoxList[0].subtitle}}</h6>
-                    <h2 class="pt-3 pb-5">{{shopBoxList[0].title}}</h2>
+                    <h2 class="text-white pt-3 pb-5">{{shopBoxList[0].title}}</h2>
                     <button type="button" class="btn btn-light">{{shopBoxList[0].btnCta}}</button>
                 </div>
             </div>
@@ -13,7 +13,7 @@
             <div class="cdShopBox cdShopBoxRight">
                 <div class="cdOverlay text-white d-flex flex-column justify-content-center align-items-center">
                     <h6>{{shopBoxList[1].subtitle}}</h6>
-                    <h2 class="pt-3 pb-5">{{shopBoxList[1].title}}</h2>
+                    <h2 class="text-white pt-3 pb-5">{{shopBoxList[1].title}}</h2>
                     <button type="button" class="btn btn-light">{{shopBoxList[1].btnCta}}</button>
                 </div>
             </div>
