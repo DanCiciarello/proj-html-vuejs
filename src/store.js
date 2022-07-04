80468bfd254c517c7cee4c0706f89d10d6295c18
DanCiciarello <me@danieleciciarello.com>
2022-07-04 10:43:30 +0200

    Update stored data

--- src/store.js
+++ src/store.js
@@ -7,18 +7,22 @@ export const state = Vue.observable({
         {
             name: "Bed",
             quantity: 6,
+            img: require("@/assets/img/product-9-500x500.jpg"),
         },
         {
             name: "Food",
             quantity: 3,
+            img: require("@/assets/img/product-3-500x500.jpg"),
         },
         {
             name: "Toys",
             quantity: 12,
+            img: require("@/assets/img/product-2-500x500.jpg"),
         },
         {
             name: "Transport",
             quantity: 10,
+            img: require("@/assets/img/product-4-500x500.jpg"),
         }
     ],
 })
