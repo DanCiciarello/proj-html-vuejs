46383a763768afe14c766eb5bdc04d342693fc7f
DanCiciarello <me@danieleciciarello.com>
2022-07-04 13:26:00 +0200

    Update Shop Card component

--- src/components/ShopCards.vue
+++ src/components/ShopCards.vue
@@ -1,12 +1,41 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-4" v-for="(card, i) in shopCardsList" :key="i">
+                    <ShopCardsItem  :shop-card-name="shopCardsList[i].name"
+                                    :shop-card-description="shopCardsList[i].description"
+                                    :shop-card-img="shopCardsList[i].img"
+                                    :shop-card-btn-cta="shopCardsList[i].btnCta" />
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
+
+// Import
+import ShopCardsItem from './ShopCardsItem.vue';
+import {state} from "../store";
+
+// Export
 export default {
 
     // Name
     name: "ShopCards",
+
+    // Components
+    components: { ShopCardsItem },
+
+    // Computed
+    computed: {
+        shopCardsList(){
+            return state.shopCardsList;
+        }
+    }
 
 }
 </script>
---
+++ src/components/ShopCardsItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        
+        <div class="cdShopCard d-flex flex-column justify-content-center align-items-center">
+            <h2 class="text-white">{{shopCardName}}</h2>
+            <span class="cdShopCardDescription">{{shopCardDescription}}</span>
+            <img :src="shopCardImg" :alt="shopCardName" class="cdShopCardImg">
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+
+    // Name
+    name: "ShopCardsItem.vue",
+
+    // Props
+    props: {
+        shopCardName: String,
+        shopCardDescription: String,
+        shopCardImg: String,
+        shopCardBtnCta: String,
+    }
+
+}
+</script>
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/shopcard";
+
+</style>
