9b3a1f50a8a42a89d2d890c68de4a137a422df10
DanCiciarello <me@danieleciciarello.com>
2022-07-04 15:35:16 +0200

    Completed Testimonials component

--- src/components/UsersTestimonials.vue
+++ src/components/UsersTestimonials.vue
@@ -1,16 +1,58 @@
 <template>
-    <div></div>
+    <div>
+        
+        <div class="cdUsersTestimonials text-center">
+
+            <div class="container">
+
+                <!-- Section title -->
+                <h2 class="text-white mb-5">Users testimonials</h2>
+    
+                <!-- Testimonials quotes list -->
+                <div class="row">
+                    <div class="col-4 g-5" v-for="(testimonial, i) in usersTestimonials" :key="i">
+                        <UsersTestimonialsItem  :testimonial-quote="testimonial.quote"
+                                                :testimonial-name="testimonial.name"
+                                                :testimonial-img="testimonial.img"
+                        />
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+
+    </div>
 </template>
 
 <script>
+
+// Import
+import {state} from "../store";
+import UsersTestimonialsItem from "./UsersTestimonialsItem.vue";
+
+// Export
 export default {
 
     // Name
     name: "UsersTestimonials",
 
+    // Components
+    components: { UsersTestimonialsItem },
+    
+    // Computed
+    computed: {
+        usersTestimonials() {
+            return state.usersTestimonials;
+        }
+    },
+
 }
 </script>
 
 <style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/userstestimonials";
 
 </style>
---
+++ src/components/UsersTestimonialsItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+        <div class="testimonialImg pb-4">
+            <img :src="testimonialImg" alt="testimonialName">
+        </div>
+
+        <div class="testimonialInfo">
+            <p class="text-white">{{testimonialQuote}}</p>
+            <p class="text-white-50">{{testimonialName}}</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    // Name
+    name: "UsersTestimonialsItem",
+
+    // Props
+    props: {
+        testimonialQuote: String,
+        testimonialName: String,
+        testimonialImg: String,
+    }
+    
+}
+
+</script>
+
+<style lang="scss">
+
+@import "../assets/scss/variables";
+@import "../assets/scss/userstestimonials";
+
+</style>
