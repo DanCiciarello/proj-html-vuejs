67da5fee657223056e75ea6abf94dad79298fc59
DanCiciarello <me@danieleciciarello.com>
2022-07-04 09:52:02 +0200

    Start category component

--- src/components/BrowseCategory.vue
+++ src/components/BrowseCategory.vue
@@ -1,5 +1,21 @@
 <template>
-    <div></div>
+    <div>
+
+        <div class="container py-5">
+            <div class="row py-5">
+                <div class="col-6 offset-3 text-center">
+                    <h2 class="pb-3">Browse by category</h2>
+                    <p>Augue purus et, tincidunt condimentum mauris. At nibh rutrum mi in. Nisi, vitae interdum eleifend dui, consequat nulla rhoncus dictum. Viverra.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
@@ -11,6 +27,12 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+
+@import "../assets/scss/variables";
+    
+    h2 {
+        color: $primary;
+    }
 
 </style>
---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+
+    // Name
+    name: "CateegoryCard",
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
